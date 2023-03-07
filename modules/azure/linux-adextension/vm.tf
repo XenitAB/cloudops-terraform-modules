94b43611aef63a8476264ea692cb292ecec44871
jimmy.hagglund@xenit.se <jimmy.hagglund@xenit.se>
2023-03-07 09:52:49 +0100

    content_type keyvault secret

--- modules/azure/linux-adextension/vm.tf
+++ modules/azure/linux-adextension/vm.tf
@@ -8,7 +8,7 @@ resource "azurerm_key_vault_secret" "ssh_admin_key_secret" {
   name         = "${var.vm_config.name}-private-ssh"
   value        = trimspace(tls_private_key.ssh_admin_key.private_key_openssh)
   key_vault_id = var.key_vault_id
-  content_type = "x509"
+  content_type = "x509 string"
 }
 
 resource "azurerm_network_interface" "nic" {
